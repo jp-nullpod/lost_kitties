112f1daf58f85e1703a99f3d456164ae0c2a04da
Jp Fortuno <jp.fortuno@nullpod.com>
2023-01-07 18:47:59 +0400

    completed CRUD

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,6 @@
 class PetsController < ApplicationController
+    before_action :set_pet, only: [:show, :edit, :update, :destroy]
+
     def index
         @pets = Pet.all
     end
@@ -8,6 +10,9 @@ class PetsController < ApplicationController
     end
 
     def create
+        @pet = Pet.new(pet_params)
+        @pet.save
+        redirect_to pet_path(@pet)
     end
 
     def show
@@ -17,8 +22,22 @@ class PetsController < ApplicationController
     end
 
     def update
+        @pet.update(pet_params)
+        redirect_to pet_path(@pet)
     end
 
     def destroy
+        @pet.destroy
+        redirect_to pets_path, status: :see_other
+    end
+
+    private 
+
+    def pet_params
+        params.require(:pet).permit(:name, :description, :found_on, :species, :location)
+    end
+
+    def set_pet
+        @pet = Pet.find(params[:id])
     end
 end
