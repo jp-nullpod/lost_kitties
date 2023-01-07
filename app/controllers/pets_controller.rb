035fc8b862539e23c4db8f07dd4679b4d0f99a4e
Jp Fortuno <jp.fortuno@nullpod.com>
2023-01-07 18:25:31 +0400

    added simple form and new view

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,24 @@
 class PetsController < ApplicationController
+    def index
+        @pets = Pet.all
+    end
+
+    def new
+        @pet = Pet.new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
